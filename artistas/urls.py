d13dfcac23adff0cb5dc64d86f0844df06648325
Ilya <137123870+ilya-ren@users.noreply.github.com>
2024-01-29 01:46:25 -0300

    Formularios editar, aliminar y agregar listos

--- artistas/urls.py
+++ artistas/urls.py
@@ -11,6 +11,8 @@ urlpatterns= [
 
     path('artistasAdd', views.artistasAdd, name='artistasAdd'),
     path('artistas_del/<str:pk>', views.artistas_del, name='artistas_del'),
+    path('artistas_finEdit/<str:pk>', views.artistas_finEdit, name='artistas_finEdit'),
+    path('artistasUpdate', views.artistasUpdate, name='artistasUpdate'),
    
 ]
 
--- artistas/views.py
+++ artistas/views.py
@@ -27,5 +27,106 @@ def artistasAdd(request):
         generos= Genero.objects.all()
         context={'generos':generos}
         return render(request, 'artistas/artistas_add.html', context)
+    else:
+        #en caso de ser POST se recuperan los datos del formulario y se graban en la tabla
+        rut        = request.POST["rut"]
+        nombre     = request.POST["nombre"]
+        aPaterno   = request.POST["ap_paterno"]
+        aMaterno   = request.POST["ap_materno"]
+        fechaNac   = request.POST["fec_nacimiento"]
+        genero     = request.POST["genero"]
+        telefono   = request.POST["telefono"]
+        email      = request.POST["email"]
+        direccion  = request.POST["direccion"]
+        activo=1
+
+        objGenero= Genero.objects.get(id_genero= genero)
+        obj= Artista.objects.create(rut=rut,
+                                    nombre=nombre,
+                                    ap_paterno=aPaterno,
+                                    ap_materno=aMaterno,
+                                    fec_nacimiento=fechaNac,
+                                    id_genero=obj.genero,
+                                    telefono=telefono,
+                                    email=email,
+                                    direccion=direccion,
+                                    activo=1)
+        obj.save()
+        context={'mensaje':"Datos grabados con exito!"}
+        return render(request, 'artistas/artistas_add.html', context)
+    
+
+def artistas_del(request,pk):
+    context={}
+    try:
+        artista= Artista.objects.get(rut=pk)
+
+        artista.delete()
+        mensaje = "Registro del artista eliminado con exito."
+        artista = Artista.objects.all()
+        context= {'artistas': artistas, 'mensaje': mensaje}
+        return render(request, 'artistas/artistas_list.html', context)
+    except:
+        mensaje= "No existe artista registrado con ese rut."
+        artistas= Artista.objects.all()
+        context= {'artista': artistas, 'mensaje': mensaje}
+        return render(request, 'artista/artistas_list.html', context)
+    
+def artistas_finEdit(request, pk):
+    if pk != "":
+        artista= Artista.objects.get(rut=pk)
+        generos= Genero.objects.all()
+
+        print(type(artista.id_genero.genero))
+
+        context={'artista': artista, 'generos':generos}
+        if artista:
+            return render(request, 'artistas/artistas_edit.html', context)
+        else:
+            context={'mensaje': "Error, el rut no existe."}
+            return render (request, 'artistas/artistas_list.html', context)
+        
+def artistasUpdate(request):
+    if request.method == "POST":
+        rut       = request.POST["rut"]
+        nombre    = request.POST["nombre"]
+        aPaterno  = request.POST["ap_paterno"]
+        aMaterno  = request.POST["ap_materno"]
+        fechaNac  = request.POST["fechaNac"]
+        genero    = request.POST["genero"]
+        telefono  = request.POST["telefono"]
+        email     = request.POST["email"]
+        direccion = request.POST["direccion"]
+        activo    = "1"
+
+        objGenero= Genero.objects.get(id_genero= genero)
+
+        artista= artista()
+        artista.rut= rut
+        artista.nombre= nombre
+        artista.ap_paterno= aPaterno
+        artista.ap_materno= aMaterno
+        artista.fechaNac= fechaNac
+        artista.id_genero=objGenero
+        artista.telefono= telefono
+        artista.email=email
+        artista.direccion=direccion
+        artista.activo=1
+        artista.save()
+
+        generos=Genero.objects.all()
+        context={'mensaje': "Datos del artista actualizados.", 'generos': generos, 'artista': artista}
+        return render(request, 'artistas/artistas_edit.html', context)
+    
+
+    else:
+        artistas = Artista.objects.all()
+        context={'artistas': artistas}
+        return render(request, 'artistas/artistas_list.html', context)
+    
+
+
+        
+
     
 
